f20b822e9a143304e15bd1253f664a881244ae19
villagearun <30830178+villagearun@users.noreply.github.com>
2017-09-12 16:15:03 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char s;
+printf("enter the letter:");
+scanf("%c",&s);
+if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u')
+{
+printf("It is vowel");
+}
+else
+{
+printf("not avowel");
+}
+}
