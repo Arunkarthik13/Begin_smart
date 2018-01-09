fd30831a5a0332c787d13617fc00fdc49aab2434
villagearun <30830178+villagearun@users.noreply.github.com>
2018-01-09 17:01:50 +0530

    Create interval.c

---
+++ interval.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main() {
+    int a,b,i;
+    printf("Enter the two intervals:");
+    scanf("%d%d",&a,&b);
+    for(i=a;i=b;i++)
+    {
+        if((i%2)==0)
+        {
+            printf("even");
+        }
+else
+{
+    printf(odd number %d",i);
+
+}
+        
+    }
+}
