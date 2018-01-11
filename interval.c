55e045b490e574a864cf784e1f3ada6a57ba5b5c
villagearun <30830178+villagearun@users.noreply.github.com>
2018-01-11 16:04:52 +0530

    Update interval.c

--- interval.c
+++ interval.c
@@ -1,20 +1,15 @@
 #include<stdio.h>
 
 void main() {
-    int a,b,i;
-    printf("Enter the two intervals:");
-    scanf("%d%d",&a,&b);
-    for(i=a;i=b;i++)
-    {
-        if((i%2)==0)
-        {
-            printf("even");
-        }
-else
-{
-    printf(odd number %d",i);
-
-}
-        
-    }
+   int a,b,i;
+   printf("Enter the two limits:");
+   scanf("%d%d",&a,&b);
+   for(i=a;i<=b;i++)
+   {
+       if((i%2)==0)
+       {
+           printf("The even numberds are :%d",i);
+          printf("\n");
+                 }
+   }
 }
