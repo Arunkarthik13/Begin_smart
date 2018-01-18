2414b672ebbe8e88840122039284e75e3f49ab82
villagearun <30830178+villagearun@users.noreply.github.com>
2018-01-18 16:27:47 +0530

    Create sorting.c

---
+++ sorting.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void main() 
+{
+    int a[10],n,i,j;
+    printf("Enter the total element");
+    scanf("%d",&n);
+    printf("\n");
+    printf("Enter the array values:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+for(i=0;i<n-1;i++)
+{
+    int temp;
+    for(j=i+1;j<n;j++)
+    {
+      if(a[i]>a[j])  
+      {
+          temp=a[i];
+          a[i]=a[j];
+          a[j]=temp;
+      }
+    }
+    }
+    printf("The sorted array is:");
+for(i=0;i<n;i++)
+{
+    printf("%d",a[i]);
+    printf("\t");
+}
+}
