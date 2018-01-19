536c6de2b145c09883c0f10838cd2ad029014607
villagearun <30830178+villagearun@users.noreply.github.com>
2018-01-19 08:10:03 +0000

    Create vowl.c

---
+++ vowl.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char r;
+printf("enter the letter:");
+scanf("%c",&r);
+if(r=='a'||r=='e'||r=='i'||r=='o'||r=='u')
+{
+printf("It is vowel");
+}
+else
+{
+printf("not avowel");
+}
+}
