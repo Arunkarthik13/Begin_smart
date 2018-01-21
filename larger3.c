3904d0e84b79b6b168ecaf148a471023e17ba0a1
villagearun <30830178+villagearun@users.noreply.github.com>
2018-01-21 09:54:23 +0530

    Create larger3.c

---
+++ larger3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int x,y,z;
+printf("Enter the number:");
+scanf("%d%d%d",&x,&y,&z);
+if((x>y)&&(x>z))
+{
+printf("%d",x);
+}
+else if(y>z)
+{
+printf("%d",y);
+}
+else
+printf("%d",z);
+}
