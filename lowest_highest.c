2348ceb8f7ebba5cc5c447a4c049db4e3673de82
villagearun <30830178+villagearun@users.noreply.github.com>
2018-01-25 16:37:37 +0530

    Update lowest_highest.c

--- lowest_highest.c
+++ lowest_highest.c
@@ -2,21 +2,29 @@
 
 void main() 
 {
- int a[10],n,i;
+ int a[10],n,i,j;
  printf("Enter the number:");
  scanf("%d",&n);
  printf("\n");
  printf("Enter the array:");
- for(i=0;i<10;i++)
+ for(i=0;i<n;i++)
  {
      scanf("%d",&a[i]);
  }
- for(i=0;i<10;i++)
- {
-       if(a[i]==n)
-       {
-        printf("The lowest number is:%d",a[i-1]);
-        printf("\nThe highest number is:%d",a[i+1]);
-        }
-        }
+ for(i=0;i<n-1;i++)
+{
+    int temp;
+    for(j=i+1;j<n;j++)
+    {
+      if(a[i]>a[j])  
+      {
+          temp=a[i];
+          a[i]=a[j];
+          a[j]=temp;
+      }
+    }
+}
+        printf("The lowest number is:%d",a[0]);
+        printf("\nThe highest number is:%d",a[n-1]);
+    
 }
