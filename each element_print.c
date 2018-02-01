ca38575c34cea9ecf6f5f55fe194cb298d0cac9a
villagearun <30830178+villagearun@users.noreply.github.com>
2018-02-01 08:04:07 +0000

    Update each element_print.c

--- each element_print.c
+++ each element_print.c
@@ -2,15 +2,20 @@
 
 void main()
 {
-    int n,a,r,s=0;
+    int n,a,s=0,r=0;
     printf("Enter the number:");
     scanf("\n%d",&n);
     while(n>0)
-    {
-        a=n%10;
+{
+       a=n%10;
         s=s*10+a;
-        s=s%10;
-        printf("\t%d",s);
         n=n/10;
     }
+    while(s>0)
+    {
+        r=0;
+        r=s%10;
+        printf("\t%d",r);
+        s=s/10;
+}
 }
