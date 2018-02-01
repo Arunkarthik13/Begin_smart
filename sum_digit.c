a9ea6fefaafa620f450134e1c40f652403db4d18
villagearun <30830178+villagearun@users.noreply.github.com>
2018-02-01 08:36:43 +0000

    Create sum_digit.c

---
+++ sum_digit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n,a,sum=0;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        a=n%10;
+        sum=sum+a;
+        n=n/10;
+    }
+    printf("\nsum of digit is:%d",sum);
+}
