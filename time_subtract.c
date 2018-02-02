9921bc5eb0992f588a92aa76e8d782511b4d230d
villagearun <30830178+villagearun@users.noreply.github.com>
2018-02-02 14:39:43 +0530

    Create time_subtract.c

---
+++ time_subtract.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a1,a2,b1,b2,t1,t2;
+    printf("Enter the time1:");
+    scanf("%d%d",&a1,&a2);
+    printf("\nEnter the time 2:");
+    scanf("%d %d",&b1,&b2);
+    t1=a1-b1;
+    t2=a2-b2;
+     printf("\nThe subtraction of time is is:%d %d",t1,t2);
+  
+}
