629cc05f9b1e2e5b0c20e9090ad21d3fe590ff01
villagearun <30830178+villagearun@users.noreply.github.com>
2018-02-08 16:30:17 +0530

    Create multi_13.c

---
+++ multi_13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main() 
+{
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    if((n%13)==0)
+    {
+        printf("It is multiple of 13.");
+            }
+            else
+            {
+                printf("NO");
+            }
+}
