31a6bec17e59952491e97f1d4c211035f09c1f07
villagearun <30830178+villagearun@users.noreply.github.com>
2018-02-09 14:49:13 +0530

    Update fib.c

--- fib.c
+++ fib.c
@@ -4,11 +4,12 @@ void main() {
    int f=0,s=1,n,last,i;
    printf("Enter the last:");
    scanf("%d",&last);
+   printf("%d",s);
    for(i=2;i<=last;i++)
    {
        n=f+s;
        f=s;
        s=n;
+      printf("\nThe fibanacci is:%d",n);
    }
-   printf("\nThe fibanacci is:%d",n);
-}
+ }
