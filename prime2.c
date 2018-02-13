1d65cb302af4139fa4681a972bab008bbcc557ba
villagearun <30830178+villagearun@users.noreply.github.com>
2018-02-13 16:34:10 +0530

    Create prime2.c

---
+++ prime2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main() {
+  int i,a,count=0;
+   printf("Enter the number:");
+   scanf("%d",&a);
+   for(i=2;i<a;i++)
+   {
+       if((a%i)==0)
+       count++;
+   }
+   if(count==0)
+   {
+   printf("yes prime");
+   printf("\n");
+   }
+   else
+   {
+       printf("not prime");
+   }
+}
