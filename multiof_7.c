a6593eacc100234eee33f9fdf954a4481cc14cd6
villagearun <30830178+villagearun@users.noreply.github.com>
2018-02-13 16:39:46 +0530

    Create multiof_7.c

---
+++ multiof_7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main() {
+  int a;
+   printf("Enter the number:");
+   scanf("%d",&a);
+   if((a%7)==0)
+   {
+       printf("\nIt is multiple of 7:%d",a);
+          }
+          else
+          {
+              printf("\nNot a multiple of seven:");
+          }
+}
+  
