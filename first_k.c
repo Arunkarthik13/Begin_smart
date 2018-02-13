838cd3c79c0aa529e80e1767993482990e0a9ba1
villagearun <30830178+villagearun@users.noreply.github.com>
2018-02-13 15:48:55 +0530

    Create first_k.c

---
+++ first_k.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+  char a[10];
+  int i,k;
+  printf("Enter the string:&k value:");
+  scanf("%s %d",&a,&k);
+  printf("\n");
+  for(i=0;i<k;i++)
+  {
+      printf("%c",a[i]);
+  }
+  
+}
