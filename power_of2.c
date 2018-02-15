72c5a8078c07f1ef404d7f5a07dd3618a4f666e4
villagearun <30830178+villagearun@users.noreply.github.com>
2018-02-15 16:57:36 +0530

    Create power_of2.c

---
+++ power_of2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main() 
+{
+ int n,a=1,p=2,i;
+ printf("Entre the number:");
+ scanf("%d ",&n);
+ for(i=1;i<=n;i++)
+ {
+     a=a*2;
+     if(a==n)
+     {
+         printf("\nThe number is  power of 2");
+ }
+
+ }
+}
+ 
+ 
