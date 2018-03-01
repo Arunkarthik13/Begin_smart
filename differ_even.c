daa0ba1b0d20a1597ee67327c28f1f78e068dfc9
villagearun <30830178+villagearun@users.noreply.github.com>
2018-03-01 16:33:37 +0530

    Create differ_even.c

---
+++ differ_even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,b,i;
+printf("Enter the numbers:");
+scanf("%d %d",&n,&b);
+int a=n-b;
+if((a%2)==0)
+{
+    printf("\n Even");
+    }
+ else
+ {
+     printf("\nodd");
+ }
+}
