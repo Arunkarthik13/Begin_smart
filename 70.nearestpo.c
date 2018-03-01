22e01603dcf86f87191ac323342217f64f191a59
villagearun <30830178+villagearun@users.noreply.github.com>
2018-03-01 16:58:09 +0530

    Create 70.nearestpo.c

---
+++ 70.nearestpo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,b=2,i,c=0,x=1;
+printf("Enter the numbers:");
+scanf("%d ",&n);
+while(n!=1)
+{
+    n=n/2;
+    c++;
+}
+for(i=1;i<=c+1;i++)
+{
+    x=i*b;
+}
+printf("\nThe nearest power of 2 is:%d",x);
+}
