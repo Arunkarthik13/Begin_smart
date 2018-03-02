18c9cc257b5700496de0093ee20e3920d3b13e4d
villagearun <30830178+villagearun@users.noreply.github.com>
2018-03-02 14:19:53 +0530

    Update cuboid.c

--- cuboid.c
+++ cuboid.c
@@ -9,7 +9,9 @@ cub(l1,b1,h1);
 }
 void cub(int l1,int b1,int h1)
 {
-    int v;
+    int v,s;
     v=l1*b1*h1;
+    s=2(l1*b1+b1*h1+h1*l1);
     printf("\nThe volume is:%d",v);
+    printf("\nThe surface area is:%d",s);
 }
