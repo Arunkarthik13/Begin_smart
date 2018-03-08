c7b2e286e3b9170f3ae514fcec53870af09d4cf0
villagearun <30830178+villagearun@users.noreply.github.com>
2018-03-08 15:52:14 +0530

    Create nolines_34.c

---
+++ nolines_34.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main() {
+   char b[100];
+   int count=0,i;
+   printf("Enter the string:");
+   scanf("%[^\n]s",&b);
+   for(i=0;b[i]!='\0';i++)
+   {
+       if(b[i]=='.')
+       {
+           count++;}
+       }
+       printf("\nThe number of words in agiven line is:%d",count+1);
+}
