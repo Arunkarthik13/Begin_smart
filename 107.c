d6856f83681f798660cc6163362dbcafb2cf3265
villagearun <30830178+villagearun@users.noreply.github.com>
2018-03-08 16:47:09 +0530

    Create 107.c

---
+++ 107.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main() 
+{
+    int a,b,c;
+    float re;
+    printf("Enter the number:");
+    scanf("%d %d %d",&a,&b,&c);
+   re=(a*b)/c;
+        printf("\n%.2f",re);
+    }
