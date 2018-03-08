da3db3b582f9895893a08876b3b047811d620f38
villagearun <30830178+villagearun@users.noreply.github.com>
2018-03-08 16:32:58 +0530

    Create maximum10.c

---
+++ maximum10.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main() 
+{
+    int a[10],i,j;
+    printf("Enter the array values:");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+for(i=0;i<9;i++)
+{
+    int temp;
+    for(j=i+1;j<10;j++)
+    {
+      if(a[i]>a[j])  
+      {
+          temp=a[i];
+          a[i]=a[j];
+          a[j]=temp;
+      }    }    }
+    printf("The sorted array is:");
+for(i=0;i<10;i++)
+{
+    printf("%d",a[i]);
+    printf("\t");
+}
+printf("\nThe maximum among 10 numbers are:%d",a[9]);
+}
