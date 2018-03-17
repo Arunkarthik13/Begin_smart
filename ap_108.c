40b34ec2f064f657e841d56615a2e62b01e35df4
villagearun <30830178+villagearun@users.noreply.github.com>
2018-03-17 13:52:50 +0530

    Create ap_108.c

---
+++ ap_108.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int a,n,d,ap;
+    printf("Enter the number:\n");
+    scanf("%d %d %d",&a,&d,&n);
+   ap=a+((n-1)*d);
+        printf("The AP value is:%d",ap);
+}
